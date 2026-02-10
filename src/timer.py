86f974b3c73d3ba9e777287462d5a633df7351a5
j3arch <j3archx@gmail.com>
2026-02-10 23:43:03 +0300

    countdown basic

--- src/timer.py
+++ src/timer.py
@@ -1,1 +1,16 @@
-# timer
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        with open("timer.txt", "w", encoding="utf-8") as f:
+            f.write(timer)
+        time.sleep(1)
+        t -= 1
+
+    print("Stream ended!")
+
+t = input("Enter the time in seconds: ")
+
+countdown(int(t))
