521a014b3fcfa57e6ec6733f0ae86ff8d43796e7
j3arch <j3archx@gmail.com>
2026-02-13 22:13:39 +0300

    timer progress

--- src/timer.py
+++ src/timer.py
@@ -2,9 +2,21 @@ import time
 import datetime as dt
 
 class Timer:
-    def __init__(self):
-        pass
-    # setup the timer class be able to operate it (add time from events)
+    def __init__(self, initial_seconds: int):
+        self.remaining_seconds = initial_seconds
+
+    def tick(self) -> None:
+        if self.remaining_seconds > 0:
+            self.remaining_seconds -= 1
+
+    def is_finished(self):
+        return self.remaining_seconds <= 0
+
+    def add_seconds(self, amount: int):
+        self.remaining_seconds += amount
+
+    def format(self):
+        return str(dt.timedelta(seconds=self.remaining_seconds))
 
 def countdown(t):
     while t:
