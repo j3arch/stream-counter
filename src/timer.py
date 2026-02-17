023ff0e584bd55d9300ef0004ba43f5ccaf7543b
j3arch <j3archx@gmail.com>
2026-02-17 23:25:52 +0300

    input and return types for clatiry

--- src/timer.py
+++ src/timer.py
@@ -2,20 +2,20 @@ import time
 import datetime as dt
 
 class Timer:
-    def __init__(self, initial_seconds: int):
+    def __init__(self, initial_seconds: int) -> None:
         self.remaining_seconds = initial_seconds
 
     def tick(self) -> None:
         if self.remaining_seconds > 0:
             self.remaining_seconds -= 1
 
-    def is_finished(self):
+    def is_finished(self) -> bool:
         return self.remaining_seconds <= 0
 
-    def add_seconds(self, amount: int):
+    def add_seconds(self, amount: int) -> None:
         self.remaining_seconds += amount
 
-    def format(self):
+    def format(self) -> str:
         return str(dt.timedelta(seconds=self.remaining_seconds))
 
 def countdown(initial_seconds: int):
