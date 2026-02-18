d81deba4f5767d69c8b504a10c09143d4dd32d4f
j3arch <j3archx@gmail.com>
2026-02-18 21:36:36 +0300

    main test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-def main():
-    pass
+import os
+import asyncio
+from dotenv import load_dotenv
+from twitchio.ext import commands
+from timer_service import Timer
+from timer_service import countdown
+
+async def main():
+    user_input = input("Enter the time in seconds: ")
+
+    countdown(int(user_input))
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
