768585e1961b29628cd2982adec64f5a68eaf24e
j3arch <j3archx@gmail.com>
2026-03-02 22:40:17 +0300

    fixed the typo

--- src/socket_service/event_listener.py
+++ src/socket_service/event_listener.py
@@ -4,12 +4,12 @@ from timer_service import Timer
 
 sio = socketio.AsyncClient() # socket.io client
 
-def setup_socker(timer: Timer) -> None:
+def setup_socket(timer: Timer) -> None:
     pass
 
 
 async def streamlabs_event_listener(timer: Timer) -> None: 
-    setup_socker(timer)  #connects to the websocket and waits for the events
+    setup_socket(timer)  #connects to the websocket and waits for the events
 
     url = f"https://sockets.streamlabs.com?token={config.STREAMLABS_SOCKET_TOKEN}"
 
