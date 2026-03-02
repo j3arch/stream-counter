4ac5872f1ddb32a71d1760525ff310dfb49f4d32
j3arch <j3archx@gmail.com>
2026-03-02 22:30:47 +0300

    update

--- src/socket_service/event_listener.py
+++ src/socket_service/event_listener.py
@@ -1,8 +1,16 @@
 import socketio
-# import config
+import config
 from timer_service import Timer
 
-sio = socketio.AsyncClient()
+sio = socketio.AsyncClient() # socket.io client
 
 def setup_socker(timer: Timer) -> None:
     pass
+
+
+async def streamlabs_event_listener(timer: Timer) -> None:
+    setup_socker(timer)
+    url = f"https://sockets.streamlabs.com?token={config.STREAMLABS_SOCKET_TOKEN}"
+
+    await sio.connect(url)
+    await sio.wait()
