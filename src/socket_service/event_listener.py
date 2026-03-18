1d082ef0689c5366f78dc49bc6e8a4417f856cd1
j3arch <j3archx@gmail.com>
2026-03-18 23:01:28 +0300

    fix

--- src/socket_service/event_listener.py
+++ src/socket_service/event_listener.py
@@ -22,10 +22,8 @@ def setup_socket(timer: Timer) -> None:
             for msg in messages:
                 bits = int(msg.get('amount', 0))
                 added = (bits // 100) * config.SECONDS_PER_100_BITS
-                timer.add_seconds(added) # placeholder, adjust price per bit
+                timer.add_seconds(added)
 
-    # setup the api calls to get the sub/bits/dono calls, calculate the amount of time that needs to be added + add it to the timer.
-    # optionally add a message of how much was added
 
         if event_type == "donation":
             for msg in messages:
