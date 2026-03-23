4b81aafbc125733fe7ac8de659087f67d6587a43
j3arch <j3archx@gmail.com>
2026-03-23 23:38:11 +0300

    custom values

--- src/config.py
+++ src/config.py
@@ -13,3 +13,9 @@ SECONDS_PER_SUB = int(os.getenv("SECONDS_PER_SUB", "60"))
 SECONDS_PER_100_BITS = int(os.getenv("SECOND_PER_100_BITS", "30"))
 
 SECONDS_PER_1_USD = int(os.getenv("SECONDS_PER_1_USD", "30"))
+
+# Custom values
+
+SECONDS_PER_5_SUBS = int(os.getenv("SECONDS_PER_5_SUBS", "420"))
+SECONDS_PER_10_SUBS = int(os.getenv("SECONDS_PER_10_SUBS", "900"))
+SECONDS_PER_20_SUBS = int(os.getenv("SECONDS_PER_20_SUBS", "1800"))
