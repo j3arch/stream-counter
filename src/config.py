4c7122f77d803b49b3a33991ca0a6f1c83699deb
j3arch <j3archx@gmail.com>
2026-03-24 00:33:31 +0300

    custom values for donations

--- src/config.py
+++ src/config.py
@@ -9,13 +9,20 @@ STREAMLABS_SOCKET_TOKEN = os.getenv("STREAMLABS_SOCKET_TOKEN")
 TWITCH_NAME = os.getenv("TWITCH_NAME")
 TWITCH_CHANNEL = os.getenv("TWITCH_CHANNEL")
 
+# Base values:
+
 SECONDS_PER_SUB = int(os.getenv("SECONDS_PER_SUB", "60"))
 SECONDS_PER_100_BITS = int(os.getenv("SECOND_PER_100_BITS", "30"))
-
 SECONDS_PER_1_USD = int(os.getenv("SECONDS_PER_1_USD", "30"))
 
-# Custom values
+# Custom sub values:
 
 SECONDS_PER_5_SUBS = int(os.getenv("SECONDS_PER_5_SUBS", "420"))
 SECONDS_PER_10_SUBS = int(os.getenv("SECONDS_PER_10_SUBS", "900"))
 SECONDS_PER_20_SUBS = int(os.getenv("SECONDS_PER_20_SUBS", "1800"))
+
+# Custom donation values:
+
+SECONDS_PER_10_USD = int(os.getenv("SECONDS_PER_10_USD", "300"))
+SECONDS_PER_25_USD = int(os.getenv("SECONDS_PER_25_USD", "900"))
+SECONDS_PER_100_USD = int(os.getenv("SECONDS_PER_100_USD", "3600"))
