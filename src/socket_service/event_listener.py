c4801bad6fd891bdda1be2a65db05296467e0b58
j3arch <j3archx@gmail.com>
2026-04-10 22:45:26 +0300

    new events placeholders

--- src/socket_service/event_listener.py
+++ src/socket_service/event_listener.py
@@ -47,7 +47,13 @@ def setup_socket(timer: Timer) -> None:
                 timer.add_seconds(config.BONUS_SECONDS_PER_5_SUBS)
                 print("Processed 5 subs! Added 120s bonus!")
 
-            
+        
+        if event_type == "Tier 2 sub":
+            pass
+
+        
+        if event_type == "Tier 3 sub":
+            pass
 
 
         if event_type == "donation":
