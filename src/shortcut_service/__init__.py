2cd8abacd3c5b15ec75101ffff39138619c32a03
j3arch <j3archx@gmail.com>
2026-04-22 00:22:48 +0300

    init update

--- src/shortcut_service/__init__.py
+++ src/shortcut_service/__init__.py
@@ -1,3 +1,4 @@
 from .keyboard_shortcuts import keyboard_shortcuts_listener
+from .terminal_input import terminal_controller
 
-__all__ = ["keyboard_shortcuts_listener"]
+__all__ = ["keyboard_shortcuts_listener", "terminal_controller"]
