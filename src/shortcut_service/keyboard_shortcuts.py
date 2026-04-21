f81f264b0e8620d799d55a467a22d1e46886adf1
j3arch <j3archx@gmail.com>
2026-04-21 23:06:20 +0300

    keyboard shortcuts test

--- src/shortcut_service/keyboard_shortcuts.py
+++ src/shortcut_service/keyboard_shortcuts.py
@@ -1,6 +1,8 @@
-from pynput import keyboard
-from timer_service import timer
+import keyboard
+from timer_service import Timer
 import asyncio
 
-def keyboard_shortcuts_listener():
-    return 0
+def keyboard_shortcuts_listener(timer: Timer, loop: asyncio.AbstractEventLoop) -> None:
+
+    keyboard.add_hotkey("F12", lambda: loop.call_soon_threadsafe(timer.add_seconds, 60))
+    print("keyboard shortcut added 60 seconds")
