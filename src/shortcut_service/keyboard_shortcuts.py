d9ddaf6c0e81051b9ea1c8999cad69cc54720744
j3arch <j3archx@gmail.com>
2026-04-22 00:06:22 +0300

    shorcuts update with pynput

--- src/shortcut_service/keyboard_shortcuts.py
+++ src/shortcut_service/keyboard_shortcuts.py
@@ -1,8 +1,22 @@
-import keyboard
+from pynput import keyboard
 from timer_service import Timer
 import asyncio
 
 def keyboard_shortcuts_listener(timer: Timer, loop: asyncio.AbstractEventLoop) -> None:
 
-    keyboard.add_hotkey("F12", lambda: loop.call_soon_threadsafe(timer.add_seconds, 60))
-    print("keyboard shortcut added 60 seconds")
+    def on_press(key):
+        try:
+            if key == keyboard.Key.page_up:
+                loop.call_soon_threadsafe(timer.add_seconds, 60)
+                print("Manually added 60s")
+            
+            if key == keyboard.Key.page_down:
+                loop.call_soon_threadsafe(timer.add_seconds, -60)
+                print("Manually removed 60s")
+        
+        except AttributeError:
+            pass
+
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()
+        print("shortcuts active")
