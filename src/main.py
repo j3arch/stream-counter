a27466a1f62782741364d3e43f7a7fc8efcd1a7c
j3arch <j3archx@gmail.com>
2026-04-24 21:28:20 +0300

    terminal commands

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 from timer_service import Timer, run_timer
 from socket_service import streamlabs_event_listener
-from shortcut_service import keyboard_shortcuts_listener
+from shortcut_service import keyboard_shortcuts_listener, terminal_controller
 
 async def main() -> None:
     initial_time = 7200
@@ -13,7 +13,8 @@ async def main() -> None:
 
     await asyncio.gather(
         run_timer(shared_timer),
-        streamlabs_event_listener(shared_timer)
+        streamlabs_event_listener(shared_timer),
+        terminal_controller(shared_timer),
     )
 
 if __name__ == "__main__":
