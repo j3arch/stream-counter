8d65c31cd892576a1c8ac09b8b27b073b89ad5fc
j3arch <j3archx@gmail.com>
2026-05-13 22:40:50 +0300

    test setup

--- src/shortcut_service/keyboard_shortcuts_test.py
+++ src/shortcut_service/keyboard_shortcuts_test.py
@@ -1,1 +1,11 @@
 import unittest
+
+from keyboard_shortcuts import keyboard_shortcuts_listener
+
+class TestShortcuts(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
