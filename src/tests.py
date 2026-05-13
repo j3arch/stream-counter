a83ab0cd8850e65a032c5cfd47e58aad52cdc8bd
j3arch <j3archx@gmail.com>
2026-05-13 23:29:48 +0300

    cleanup

--- src/tests.py
+++ src/tests.py
@@ -1,16 +1,7 @@
 import unittest
 
 class TestShortcuts(unittest.TestCase):
-    def test_pause_shortcut(self):
-        pass
-
-    def test_resume_shortcut(self):
-        pass
-
-    def test_claim_shortcut(self):
-        pass
-
-
+    pass
 
 
 if __name__ == "__main__":
