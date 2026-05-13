95d6e9cf1f1d4102537c1421a760d76b6ce6db72
j3arch <j3archx@gmail.com>
2026-05-13 22:41:29 +0300

    tests template

--- src/shortcut_service/keyboard_shortcuts_test.py
+++ src/shortcut_service/keyboard_shortcuts_test.py
@@ -3,7 +3,14 @@ import unittest
 from keyboard_shortcuts import keyboard_shortcuts_listener
 
 class TestShortcuts(unittest.TestCase):
-    pass
+    def test_pause_shortcut(self):
+        pass
+
+    def test_resume_shortcut(self):
+        pass
+
+    def test_claim_shortcut(self):
+        pass
 
 
 
