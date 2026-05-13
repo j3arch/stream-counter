ec5fee14ff15334f9bb6768fbb2fcbfdcaeedfd7
j3arch <j3archx@gmail.com>
2026-05-13 23:23:10 +0300

    general tests

---
+++ src/tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestShortcuts(unittest.TestCase):
+    def test_pause_shortcut(self):
+        pass
+
+    def test_resume_shortcut(self):
+        pass
+
+    def test_claim_shortcut(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
