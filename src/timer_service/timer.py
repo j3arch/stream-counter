652099fc68376bfeca7b0ec82d8becd43be36f28
j3arch <j3archx@gmail.com>
2026-06-05 21:42:46 +0300

    small fix

--- src/timer_service/timer.py
+++ src/timer_service/timer.py
@@ -9,7 +9,7 @@ class Timer:
         # self.state_file = "timer_state.json"
         self.remaining_seconds = initial_seconds
         self.paused = False
-        self.cap_seconds = config.TIMER_CAP_SECONDS                # Add to the notes! Delete comments after!
+        self.cap_seconds = config.TIMER_CAP_SECONDS                
         self.has_hit_cap = False
 
         # Milestone tracker (for subs)
@@ -64,7 +64,7 @@ class Timer:
             print("Cap has been reached, no more time can be added")
             return
         
-        new_total = max(0, self.remaining_seconds + amount)                 # Add to the notes! Delete comments after!
+        new_total = max(0, self.remaining_seconds + amount)                 
 
         if new_total >= self.cap_seconds:
             self.remaining_seconds = self.cap_seconds                     
